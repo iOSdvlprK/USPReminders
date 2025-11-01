16fc3a1b47c8fc39a3c9cf67e80ab0fdaab5fe13
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-11-01 22:25:27 +0900

    Initial Commit

---
+++ USPReminders/USPRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPRemindersApp.swift
+//  USPReminders
+//
+//  Created by joe on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct USPRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
